6d31b59f822d111f13f9b6686fe15fd30ab5c8eb
AN-dragon <spectdragoniv@gmail.com>
2022-11-25 11:02:16 +0000

    NavBar Completa

--- includes/header.php
+++ includes/header.php
@@ -17,8 +17,15 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav                       "
-
+                        <a class="nav-link" aria-current="page" href="/index.php">Gêneros</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="/list_livros.php">Livros</a>
+                    </li>
+                </ul>
+        </div>
+    </nav>
+<br />
     <main class="container">
 </body>
 </html>
